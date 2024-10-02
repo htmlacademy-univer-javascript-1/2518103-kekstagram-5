069756af96fe07bc9b093cd4ceb2deddfa98a85d
Емельянов Максим <masiko-17@mail.ru>
2024-10-02 23:21:53 +0500

    Создаст функцию для проверки проведения встречи

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,51 @@
-function checkingLengthString(string, maxLength) {
-  return string.length <= maxLength;
+
+// Создает объект, в котором ключи это часы и минуты
+function createsTimeArray(timeLine) {
+  timeLine = timeLine.split(':');
+  const arrayHoursAndMinutes = [];
+  for (let i = 0; i < timeLine.length; i++) {
+    const newNumber = parseInt(timeLine[i], 10);
+    arrayHoursAndMinutes.push(newNumber);
+  }
+  return {
+    hours: arrayHoursAndMinutes[0],
+    minutes: arrayHoursAndMinutes[1],
+  };
 }
 
-function checkingStringPalindrome(string) {
-  const currentStr = (string.replaceAll(' ', '')).toLowerCase();
-  let newStr = '';
-  for (let i = (currentStr.length - 1); i >= 0; i--) {
-    newStr += currentStr[i];
+// Рассчитывает время окончания встречи
+function calculateTheEndTimeMeeting(hoursStartMeeting, minutesStartMeeting, durationMeeting) {
+  const meetingTheEndTime = {
+    hours: 0,
+    minutes: 0,
+  };
+
+  if (minutesStartMeeting + durationMeeting <= 59) {
+    meetingTheEndTime.hours = hoursStartMeeting;
+    meetingTheEndTime.minutes = minutesStartMeeting + durationMeeting;
+  } else {
+    meetingTheEndTime.hours = (hoursStartMeeting + Math.floor((minutesStartMeeting + durationMeeting) / 60));
+    meetingTheEndTime.minutes = (minutesStartMeeting + durationMeeting) - (Math.floor((minutesStartMeeting + durationMeeting) / 60) * 60);
   }
 
-  return newStr === currentStr;
+  return meetingTheEndTime;
 }
+
+// Проверяет можно ли провести встречу в рабочее время
+function itPossibleHoldMeeting(timeBeginning, timeTheEnd, meetingStart, durationMeeting) {
+  timeBeginning = createsTimeArray(timeBeginning);
+  timeTheEnd = createsTimeArray(timeTheEnd);
+  meetingStart = createsTimeArray(meetingStart);
+
+  if (timeBeginning.hours <= meetingStart.hours && timeBeginning.minutes <= meetingStart.minutes) {
+    const timeTheEndMeeting = calculateTheEndTimeMeeting(meetingStart.hours, meetingStart.minutes, durationMeeting);
+    if (timeTheEndMeeting.hours <= timeTheEnd.hours && timeTheEndMeeting.minutes <= timeTheEnd.minutes) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
