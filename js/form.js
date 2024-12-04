2fb135bff04051d40e7c99b486dc7260a212737c
Емельянов Максим <masiko-17@mail.ru>
2024-12-04 20:47:14 +0500

    Добавить возможность редактирования фотографии

--- js/form.js
+++ js/form.js
@@ -13,8 +13,93 @@ const pristine = new Pristine(imageForm, {
   errorTextTag: 'p',
   errorTextClass: 'form__error'
 }, true);
+const imagePreview = imageForm.querySelector('#preview');
+const scaleAddButton = imageForm.querySelector('.scale__control--bigger');
+const scaleDecreaseButton = imageForm.querySelector('.scale__control--smaller');
+const effectLevel = imageForm.querySelector('.effect-level__value');
+const DEFAULT_VOLUME = 100;
+const filterButtonList = document.querySelector('.effects__list');
+const SCALE_DIFFERENCE = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+const sliderElement = document.querySelector('.effect-level__slider');
+
+class Filter {
+  constructor(name, min, max, step) {
+    this.name = name;
+    this.min = min;
+    this.max = max;
+    this.step = step;
+  }
+}
+
+const FILTERS = {
+  'effect-chrome': new Filter('grayscale', 0, 1, 0.1),
+  'effect-sepia': new Filter('sepia', 0, 1, 0.1),
+  'effect-marvin': new Filter('invert', 0, 1, 0.01),
+  'effect-phobos': new Filter('blur', 0, 3, 0.1),
+  'effect-heat': new Filter('brightness', 1, 3, 0.1)
+};
+
+const operateScale = (evt) => {
+  let scale = parseInt(scaleOutput.value, 10);
+  if (evt.target.classList.contains('scale__control--bigger') && scale + SCALE_DIFFERENCE <= MAX_SCALE) {
+    scale += SCALE_DIFFERENCE;
+  } else if (evt.target.classList.contains('scale__control--smaller') && scale - SCALE_DIFFERENCE >= MIN_SCALE) {
+    scale -= SCALE_DIFFERENCE;
+  }
+
+  scaleOutput.value = `${scale}%`;
+  imagePreview.style.transform = `scale(${scale / 100})`;
+};
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100
+  },
+  step: 1,
+  start: 100,
+  connect: 'lower'
+});
+
+const operateSliderValue = (filter) => {
+  if (filter.name === 'blur') {
+    return `${sliderElement.noUiSlider.get()}px`;
+  }
+
+  return `${sliderElement.noUiSlider.get()}`;
+};
+
+const onFilterClick = (evt) => {
+  if (evt.target.matches('input[type=radio]')) {
+    if (evt.target.id !== 'effect-none') {
+      sliderElement.parentElement.classList.remove('hidden');
+      const filter = FILTERS[`${evt.target.id}`];
+      sliderElement.noUiSlider.updateOptions({
+        range: {
+          min: filter.min,
+          max: filter.max
+        },
+        step: filter.step,
+        start: filter.max,
+      });
+      sliderElement.noUiSlider.on('update', () => {
+        imagePreview.style.filter = `${filter.name}(${operateSliderValue(filter)})`;
+        effectLevel.value = operateSliderValue(filter) / filter.max * 100;
+      });
+    } else {
+      sliderElement.parentElement.classList.add('hidden');
+      imagePreview.style.filter = '';
+      effectLevel.value = DEFAULT_VOLUME;
+    }
+  }
+};
+
+const addFilters = () => filterButtonList.addEventListener('click', onFilterClick);
 
 const validateHashTagsField = (value) => {
+  hashTagsField.value = value.trim();
   const hashtags = value.toLowerCase()
     .split(' ')
     .filter((x) => x);
@@ -35,7 +120,8 @@ const onDocumentKeydown = (evt) => {
 };
 
 const validateComments = (value) => {
-  descriptionField.value = value.trimStart();
+  value = value.trim();
+  descriptionField.value = value;
   return value.length <= 140;
 };
 
@@ -46,6 +132,9 @@ const openModal = () => {
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
   exitButton.addEventListener('click', closeModal);
+  scaleAddButton.addEventListener('click', operateScale);
+  scaleDecreaseButton.addEventListener('click', operateScale);
+  addFilters();
 };
 
 function closeModal() {
@@ -57,6 +146,9 @@ function closeModal() {
   scaleOutput.value = '100%';
   document.removeEventListener('keydown', onDocumentKeydown);
   exitButton.removeEventListener('click', closeModal);
+  scaleAddButton.addEventListener('click', operateScale);
+  scaleDecreaseButton.addEventListener('click', operateScale);
+  filterButtonList.removeEventListener('click', onFilterClick);
 }
 
 uploadInput.addEventListener('change', (evt) => {
